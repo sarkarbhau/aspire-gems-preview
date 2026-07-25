573cf8cef2c2392d8fee6727a4eb7b10645696d4
sarkarbhau <gifboi30@gmail.com>
2026-07-25 23:09:13 +0530

    Add Sanity CLI configuration

---
+++ cms/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'REPLACE_WITH_FRIEND_OWNED_PROJECT_ID',
+    dataset: process.env.SANITY_STUDIO_DATASET || 'production',
+  },
+})
