d68cf766f28006a2e1cd9197cbf63193c8483dac
sarkarbhau <gifboi30@gmail.com>
2026-07-26 12:48:09 +0530

    Connect Sanity CLI to friend-owned project

--- cms/sanity.cli.js
+++ cms/sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'REPLACE_WITH_FRIEND_OWNED_PROJECT_ID',
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID || '3u0i5ljy',
     dataset: process.env.SANITY_STUDIO_DATASET || 'production',
   },
 })
